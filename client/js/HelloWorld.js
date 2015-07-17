382fd561dd6e3a25c614c67dc240d590f247d71e
Samnang Chhun <samnang.chhun@gmail.com>
2015-07-17 13:48:36 +0700

    Get it working!!!

--- client/js/HelloWorld.js
+++ client/js/HelloWorld.js
@@ -1,11 +1,5 @@
 import React from 'react';
 
-//function renderReactComponent(componentClass, props) {
-//  return React.renderToString(
-//    React.createElement(global[componentClass], props)
-//  );
-//}
-
 class HelloWorld extends React.Component {
   render() {
     return React.createElement("h1", null, "Hello ", this.props.name);
@@ -13,5 +7,4 @@ class HelloWorld extends React.Component {
   }
 }
 
-//global.HelloWorld = HelloWorld;
-//global.renderReactComponent = renderReactComponent;
+export default HelloWorld;
