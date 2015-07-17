6c5e69b9240e5b2dfdd028e2666c34877b1e489f
Justin Gordon <justin.gordon@gmail.com>
2015-07-16 23:16:25 -0700

    justin-wip-2

---
+++ client/js/Global.js
@@ -0,0 +1,7 @@
+import HelloWorld from './HelloWorld';
+import HelloString from './HelloString';
+import React from 'react';
+
+global.HelloWorld = HelloWorld;
+global.HelloString = HelloString;
+global.React = React;
---
+++ client/js/HelloString.js
@@ -0,0 +1,7 @@
+const HelloString = {
+  world() {
+    return "Hello WORLD! Will this work??";
+  }
+};
+
+export default HelloString;
